7a7b6c0706cbf83b2e99866340ab09670deceff5
Olgeiser <olgeiser.andreas@training.lug-ag.de>
2024-12-10 14:29:15 +0100

    musik eingebunden

---
+++ FateOfTheRacoon/HintergrundMusik.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace FateOfTheRacoon
+{
+    internal class HintergrundMusik
+    {
+        public static void Mp3Abspielen(string mp3Pfad)
+        {
+            string hauptOrdner = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+            mp3Pfad = @"Hintergrundmusik\Moana.wav";
+            string ganzerPfad = Path.Combine(hauptOrdner, mp3Pfad);
+            using (var audioFile = new AudioFileReader(ganzerPfad))
+            using (var outputDevice = new WaveOutEvent())
+            {
+                outputDevice.Init(audioFile);
+                outputDevice.Play();
+                while (outputDevice.PlaybackState == PlaybackState.Playing)
+                {
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
---
+++ FateOfTheRacoon/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FateOfTheRacoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HintergrundMusik.Mp3Abspielen("");
+        }
+    }
+}
